eca8ac95f3ed0fe72f60f2aaeea3dfdd61b74c39
Veronika <58723287+interestIngc@users.noreply.github.com>
2023-04-21 15:08:05 +0300

    added another mode of local simulation

--- run.sh
+++ run.sh
@@ -1,14 +1,11 @@
 #!/bin/zsh
 
 n=70
-pids=()
 
-go run localsimulation/mainserver/*.go --log_file outputs/mainserver.txt --ip 127.0.0.1 --port 8080 --n "$n" & pids+=($!)
+go run local/distributed/mainserver/*.go --log_file outputs/mainserver.txt --ip 127.0.0.1 --port 8080 --n "$n" &
 
 for (( i = 0; i < $n; i++ ))
 do
-  go run localsimulation/node/main.go --input_file input.json --base_ip 127.0.0.1 --port 8080 --i $i \
-  --log_file "outputs/process${i}.txt" & pids+=($!)
+  go run local/distributed/node/main.go --input_file input.json --base_ip 127.0.0.1 --port 8080 --i $i \
+  --log_file "outputs/process${i}.txt" &
 done
-
-echo "${pids[@]}"
