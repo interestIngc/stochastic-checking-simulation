5bfa9e833c8cbc22bd7f70aead6a6b79ce41d6ee
Veronika <58723287+interestIngc@users.noreply.github.com>
2024-05-17 16:05:36 +0300

    command changes

--- scripts/local_run.sh
+++ scripts/local_run.sh
@@ -2,12 +2,12 @@
 
 n=$1
 
-go run ../simulation/mainserver/*.go --log_file=../outputs/mainserver.txt --base_ip=127.0.0.1 --base_port=8080 --n="$n" --nodes=1 &
+go run ../simulation/mainserver/*.go --log_file=../outputs/mainserver.txt --base_ip=127.0.0.1 --base_port=8080 --nodes=1 --n="$n" &
 
 for (( i = 0; i < $n; i++ ))
 do
-  go run ../simulation/node/*.go --input_file=../input.json --base_ip=127.0.0.1 --base_port=8080 --i="$i" \
-  --log_file="../outputs/process${i}.txt" --transactions="$3" --transaction_init_timeout_ns="$4" --nodes=1 &
+  go run ../simulation/node/*.go --input_file=../input.json --base_ip=127.0.0.1 --base_port=8080 --nodes=1 --i="$i" \
+  --log_file="../outputs/process${i}.txt" --keys_dir="../keys" --transactions="$3" --transaction_init_timeout_ns="$4" &
 done
 
 sleep "$2"
